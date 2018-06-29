a1b822ce148d6ee689ce20fb9af9b0a1bc1c0fd5
juanosarg <31105173+juanosarg@users.noreply.github.com>
2018-06-29 16:21:13 +0200

    Alpha

---
+++ Source/RimBees/RimBees/CompProperties_TreeHive.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace RimBees
+{
+    class CompProperties_TreeHive : CompProperties
+    {
+
+        public string species = "Plant_TreeOak";
+  
+
+        public CompProperties_TreeHive()
+        {
+            this.compClass = typeof(CompTreeHive);
+        }
+    }
+}
