3cc7e01b462bee3ddb4d2dede0ffdd411f813129
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-09-24 18:42:50 +0200

    v2.0

---
+++ Source/RimBees/RimBees/CompBeeResearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RimBees
+{
+    public class CompBeeResearch : CompUsable
+    {
+       
+        protected override string FloatMenuOptionLabel
+        {
+            get
+            {
+                return string.Format(base.Props.useLabel);
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            foreach (Gizmo c in base.CompGetGizmosExtra())
+            {
+                yield return c;
+            }
+            Command_Action BR_Gizmo_DestroyThis = new Command_Action();
+            BR_Gizmo_DestroyThis.action = delegate
+            {
+                this.parent.Destroy();
+            };
+            BR_Gizmo_DestroyThis.defaultLabel = "RB_DestroyResearch".Translate();
+            BR_Gizmo_DestroyThis.defaultDesc = "RB_DestroyResearchLabel".Translate();
+            BR_Gizmo_DestroyThis.icon = ContentFinder<Texture2D>.Get("ui/commands/Detonate", true);
+            yield return BR_Gizmo_DestroyThis;
+
+        }
+
+        public override void Initialize(CompProperties props)
+        {
+            base.Initialize(props);
+            
+        }
+
+        
+
+        
+    }
+}
---
+++ Source/RimBees/RimBees/GraphicsCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace RimBees
+{
+    [StaticConstructorOnStartup]
+    public static class GraphicsCache
+    {
+        public static readonly Texture2D BeeResearchNutTech = ContentFinder<Texture2D>.Get("Research/BeeResearchNutTech", true);
+        public static readonly Texture2D BeeResearchNutAdapt = ContentFinder<Texture2D>.Get("Research/BeeResearchNutAdapt", true);
+        public static readonly Texture2D BeeResearchAdaptTech = ContentFinder<Texture2D>.Get("Research/BeeResearchAdaptTech", true);
+        public static readonly Texture2D BeeResearchStoPsy = ContentFinder<Texture2D>.Get("Research/BeeResearchStoPsy", true);
+        public static readonly Texture2D BeeResearchAgriSwamp = ContentFinder<Texture2D>.Get("Research/BeeResearchAgriSwamp", true);
+        public static readonly Texture2D BeeResearchPlasAlloy = ContentFinder<Texture2D>.Get("Research/BeeResearchPlasAlloy", true);
+
+
+
+
+
+
+    }
+}
