2f377f66cb190fbe93385835da76c759bd9ea457
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-11-14 16:27:25 +0100

    v2.033

--- Source/RimBees/RimBees/HarmonyPatchers.cs
+++ Source/RimBees/RimBees/HarmonyPatchers.cs
@@ -28,7 +28,68 @@ namespace RimBees
     }
 
 
+    /*This Harmony Postfix changes terrain calculation for floating creatures
+   */
+    [HarmonyPatch(typeof(Verse.AI.Pawn_PathFollower))]
+    [HarmonyPatch("CostToMoveIntoCell")]
+    [HarmonyPatch(new Type[] { typeof(Pawn), typeof(IntVec3) })]
+    public static class Pawn_PathFollower_CostToMoveIntoCell_Patch
+    {
+        [HarmonyPostfix]
+        public static void MakeFloatingCreaturesGreatAgain(Pawn pawn, IntVec3 c, ref int __result)
 
+        {
+            if ((pawn.Map != null) && (pawn.def.defName.Contains("GR_BeeHybrid_")))
+            {
+                
+                    int num;
+                    if (c.x == pawn.Position.x || c.z == pawn.Position.z)
+                    {
+                        num = pawn.TicksPerMoveCardinal;
+                    }
+                    else
+                    {
+                        num = pawn.TicksPerMoveDiagonal;
+                    }
+                    TerrainDef terrainDef = pawn.Map.terrainGrid.TerrainAt(c);
+                    if (terrainDef == null)
+                    {
+                        num = 10000;
+                    }
+                    else if ((terrainDef.passability == Traversability.Impassable) && !terrainDef.IsWater)
+                    {
+                        num = 10000;
+                    }
+                    else if (terrainDef.IsWater)
+                    {
+                        num = 10000;
+                    }
+                    List<Thing> list = pawn.Map.thingGrid.ThingsListAt(c);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        Thing thing = list[i];
+                        if (thing.def.passability == Traversability.Impassable)
+                        {
+                            num = 10000;
+                        }
+
+                        if (thing is Building_Door)
+                        {
+                            num += 45;
+                        }
+                    }
+
+                    __result = num;
+
+                
+
+            }
+
+
+
+
+        }
+    }
 
 
     /*This Harmony Postfix allows us to regrow plants with a 25% percentage if an active beehouse is nearby
