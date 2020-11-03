5ac72d685e00b99e8968b52f6a1d2ab7361b9209
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-11-03 19:12:24 +0100

    v4.3

--- 1.2/Source/RimBees/RimBees/CompClasses/CompUseEffect_ShowBeeResearch.cs
+++ 1.2/Source/RimBees/RimBees/CompClasses/CompUseEffect_ShowBeeResearch.cs
@@ -15,6 +15,11 @@ namespace RimBees
             string nameOfTheImage = this.parent.TryGetComp<CompBeeResearchImages>().GetImage;
             string textOfTheImage = this.parent.TryGetComp<CompBeeResearchImages>().GetText;
 
+            if (user.Faction == Faction.OfPlayer)
+            {
+                user.health.AddHediff(HediffDef.Named("RB_RecentlyResearched"));
+            }
+
             beeresearch = new Dialog_BeeResearch(nameOfTheImage, textOfTheImage);
             Find.WindowStack.Add(beeresearch);
         }
