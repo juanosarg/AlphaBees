7d1958e36ca60fe94629f6b1aeb193956ff88524
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-09-10 17:02:40 +0200

    v7.0

--- 1.5/Source/RimBees/RimBees/Harmony/Plant_PlantCollected.cs
+++ 1.5/Source/RimBees/RimBees/Harmony/Plant_PlantCollected.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.SocialPlatforms;
 using Verse;
+using static UnityEngine.GraphicsBuffer;
 
 namespace RimBees
 {
@@ -19,54 +21,42 @@ namespace RimBees
     public static class Plant_PlantCollected_Patch
     {
         [HarmonyPrefix]
-        public static bool RegrowIfBeehouseNearby(ref Plant __instance)
+        public static void RegrowIfBeehouseNearby(ref Plant __instance)
         {
 
-            if (__instance.Blighted)
-                return true;
+
 
             if (__instance.def.plant.HarvestDestroys && __instance.def.plant.Sowable && !__instance.def.plant.IsTree)
 
             {
-                int num = GenRadial.NumCellsInRadius(RimBees_Settings.beeEffectRadius);
-                for (int i = 0; i < num; i++)
+                HashSet<Thing> beehouses = __instance.Map.GetComponent<Beehouses_MapComponent>().beehouses_InMap;
+
+                foreach (var beehouse in beehouses)
                 {
-                    IntVec3 current = __instance.Position + GenRadial.RadialPattern[i];
-                    if (current.InBounds(__instance.Map))
+                    if (beehouse.PositionHeld.DistanceTo(__instance.PositionHeld) <= RimBees_Settings.beeEffectRadius)
                     {
-                        Building getbeehouse = current.GetEdifice(__instance.Map);
-                        if ((getbeehouse != null) && (getbeehouse is Building_Beehouse))
+                        Building_Beehouse thebeehouse = (Building_Beehouse)beehouse;
+
+                        if (thebeehouse.BeehouseIsRunning)
                         {
-
-                            Building_Beehouse thebeehouse = (Building_Beehouse)getbeehouse;
-
-                            if (thebeehouse.BeehouseIsRunning)
+                            Random random = new Random();
+                            if (random.NextDouble() > 0.75)
                             {
-                                Random random = new Random();
-                                if (random.NextDouble() > 0.75)
-                                {
-                                    Thing thing = ThingMaker.MakeThing(ThingDef.Named(__instance.def.defName), null);
-                                    Plant plant = (Plant)thing;
-                                    GenSpawn.Spawn(plant, __instance.Position, __instance.Map);
-                                    plant.Growth = 0.25f;
-                                    __instance.Map.mapDrawer.MapMeshDirty(__instance.Position, MapMeshFlagDefOf.Things);
-                                    return true;
-                                }
+                                Thing thing = ThingMaker.MakeThing(ThingDef.Named(__instance.def.defName), null);
+                                Plant plant = (Plant)thing;
+                                GenSpawn.Spawn(plant, __instance.Position, __instance.Map);
+                                plant.Growth = 0.25f;
+                                __instance.Map.mapDrawer.MapMeshDirty(__instance.Position, MapMeshFlagDefOf.Things);
 
                             }
 
-
                         }
-
-
                     }
-
                 }
-                return true;
+
+
 
             }
-            else return true;
-
 
 
 
