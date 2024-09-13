6f53d884473ce147c6aa4f090c274fe3c26e747b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-09-13 19:48:06 +0200

    v7.0

---
+++ 1.5/Source/RimBees/RimBees/AdditionalBeeEffects/AdditionalBeeEffects_Harvest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using HarmonyLib;
+using RimWorld;
+using Verse.Sound;
+using Verse;
+
+
+
+namespace RimBees
+{
+    public class AdditionalBeeEffects_Harvest : AdditionalBeeEffects
+    {
+
+
+        public int rareTickFrequency = 10;
+        private int tickCounter = 0;
+        public PlantDestructionMode PlantDestructionMode => PlantDestructionMode.Smash;
+
+        public AdditionalBeeEffects_Harvest()
+        {
+        }
+
+        public override void AdditionalEffectTick(Building_Beehouse building)
+        {
+            if (tickCounter > rareTickFrequency)
+            {
+                if (building.Map != null)
+                {
+                    CellRect rect = GenAdj.OccupiedRect(building.Position, building.Rotation, IntVec2.One);
+                    rect = rect.ExpandedBy(RimBees_Settings.beeEffectRadius);
+                    foreach (IntVec3 current in rect.Cells)
+                    {
+                        List<Thing> plantList = current.GetThingList(building.Map);
+                        for (int i = 0; i < plantList.Count; i++)
+                        {
+                            Plant plant;
+                            if ((plant = plantList[i] as Plant) != null && plant.HarvestableNow)
+                            {
+                                HarvestPlant(plant, building);
+                                goto plantfound;
+                            }
+                        }
+
+                    }
+                    plantfound: { }
+
+                }
+                tickCounter = 0;
+            }
+            tickCounter++;
+        }
+
+
+        public void HarvestPlant(Plant plant, Building_Beehouse building)
+        {
+
+            int num = plant.YieldNow();
+
+            if (num > 0)
+            {
+                Thing thing = ThingMaker.MakeThing(plant.def.plant.harvestedThingDef);
+                thing.stackCount = num;
+
+                GenPlace.TryPlaceThing(thing, plant.Position, plant.Map, ThingPlaceMode.Near);
+
+            }
+
+            foreach (ThingComp allComp in plant.AllComps)
+            {
+                foreach (ThingDefCountClass item in allComp.GetAdditionalHarvestYield())
+                {
+                    Thing thing2 = ThingMaker.MakeThing(item.thingDef);
+                    thing2.stackCount = item.count;
+                    GenPlace.TryPlaceThing(thing2, plant.Position, plant.Map, ThingPlaceMode.Near);
+                }
+            }
+            plant.def.plant.soundHarvestFinish.PlayOneShot(building);
+            plant.Destroy();
+           
+
+        }
+
+
+    }
+}
